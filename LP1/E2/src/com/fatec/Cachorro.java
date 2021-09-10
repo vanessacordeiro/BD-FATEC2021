b77264fa86dc4ac6a92bf1edfe6c37d7ddb8fbe7
Vanessa Cordeiro Gonçalves de Oliveira <vanessa.oliveira28@fatec.sp.gov.br>
2021-09-09 23:35:36 -0300

    Classes

---
+++ LP1/E2/src/com/fatec/Cachorro.java
@@ -0,0 +1,39 @@
+package com.fatec;
+
+public class Cachorro {
+    private String nome;
+    private int idade;
+    private float peso;
+
+    public Cachorro () {}
+
+    public Cachorro (String nome, int idade, float peso) {
+        this.nome = nome;
+        this.idade = idade;
+        this.peso = peso;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+}
